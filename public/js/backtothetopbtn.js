b9d4bf5cf329c9a55cf84ab9e2ef27a76dc0aec3
Richard Sucgang <sucgangrichard_act@plmun.edu.ph>
2025-03-19 00:21:46 +0800

    STLP

---
+++ public/js/backtothetopbtn.js
@@ -0,0 +1,22 @@
+// Back to top button
+const backToTop = document.querySelector('.back-to-top');
+if (backToTop) {
+    const toggleBacktotop = () => {
+        if (window.scrollY > 100) {
+            backToTop.classList.add('active');
+        } else {
+            backToTop.classList.remove('active');
+        }
+    };
+    
+    window.addEventListener('load', toggleBacktotop);
+    document.addEventListener('scroll', toggleBacktotop);
+    
+    backToTop.addEventListener('click', function(e) {
+        e.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+}
