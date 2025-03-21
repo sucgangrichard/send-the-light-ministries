1b67df8d859f0b6264dae3f200e3607833f332d2
Richard Sucgang <sucgangrichard_act@plmun.edu.ph>
2025-03-22 00:29:24 +0800

    STLP

--- public/js/home.js
+++ public/js/home.js
@@ -1,16 +1,27 @@
 const blogSection = document.querySelector('.blogs-section');
 const blogsRow = document.getElementById('blogs-row');
+let allBlogs = []; // Stores all blogs for search functionality
 
+// Initialize and load blogs
 db.collection("blogs").get().then((blogs) => {
     blogs.forEach(blog => {
-        if (blog.id != decodeURI(location.pathname.split("/").pop())) {
-            createBlog(blog);
+        const blogId = blog.id;
+        const blogData = blog.data();
+        
+        if (blogId != decodeURI(location.pathname.split("/").pop())) {
+            allBlogs.push({ id: blogId, data: blogData });
+            createBlog(blogId, blogData);
         }
-    })
-})
+    });
+});
 
-const createBlog = (blog) => {
-    let data = blog.data();
+// Modified createBlog function
+const createBlog = (id, data) => {
+    const cleanedOverview = data.article
+        .replace(/!\[.*?\]\(.*?\)/g, '')
+        .replace(/#+\s?.*/g, '')
+        .substring(0, 200);
+
     const blogCol = document.createElement('div');
     blogCol.className = 'col-lg-4 col-md-6 mb-4';
     blogCol.innerHTML = `
@@ -19,11 +30,52 @@ const createBlog = (blog) => {
                 <img src="${data.bannerImage}" class="blog-image img-fluid" alt="${data.title}">
             </div>
             <div class="article-content">
-                <h3 class="blog-title">${data.title.substring(0, 100)}...</h3>
-                <p class="blog-overview">${data.article.substring(0, 200)}...</p>
-                <a href="/${blog.id}" class="btn-read">Read</a>
+                <p class="article-date">${data.publishedAt}</p>
+                <h3 class="blog-title">${data.title.substring(0, 100)}</h3>
+                <p class="blog-overview">${cleanedOverview}...</p>
+                <a href="/${id}" class="btn-read">Read More</a>
             </div>
         </div>
     `;
     blogsRow.appendChild(blogCol);
 };
+
+// Search functionality - works with both Enter key and button click
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+
+// Handle search input
+searchInput.addEventListener('input', function(e) {
+    performSearch(e.target.value);
+});
+
+// Handle search button click
+searchButton.addEventListener('click', () => {
+    performSearch(searchInput.value);
+});
+
+// Handle Enter key in search input
+searchInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        performSearch(searchInput.value);
+    }
+});
+
+function performSearch(searchTerm) {
+    const cleanTerm = searchTerm.toLowerCase().trim();
+    filterBlogs(cleanTerm);
+}
+
+function filterBlogs(searchTerm) {
+    blogsRow.innerHTML = ''; // Clear current results
+
+    const filteredBlogs = allBlogs.filter(blog => {
+        const titleMatch = blog.data.title.toLowerCase().includes(searchTerm);
+        const contentMatch = blog.data.article.toLowerCase().includes(searchTerm);
+        return titleMatch || contentMatch;
+    });
+
+    filteredBlogs.forEach(blog => {
+        createBlog(blog.id, blog.data);
+    });
+}
